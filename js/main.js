81501c0d35be1ce2fdb4c2e1914c4216a876c9bd
Kaio Jesus <kaioramos990@hotmail.com>
2023-04-30 23:43:34 -0300

    Aula 3 data-atributes

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 
-const controle = document.querySelectorAll(".controle-ajuste")
+const controle = document.querySelectorAll("[data-controle")
 //pega todos os elementos com a mesma classe
 
 controle.forEach( (elemento) => {
     elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.textContent, evento.target.parentNode);
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
     })
 })
 
 function manipulaDados(operacao, controle){
-    const peca = controle.querySelector('.controle-contador');
+    const peca = controle.querySelector("[data-contador]");
 
     if(operacao === "-"){
         peca.value = parseInt(peca.value) - 1;
